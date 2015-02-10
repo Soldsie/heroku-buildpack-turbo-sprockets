525815c9eacb8aa982f927edf5ee7f5f9a3a3406
dahoopster <han.phony@gmail.com>
2015-02-10 09:26:21 -0800

    adding silly obfuscation

--- spec/helpers/soldsie_s3_helper_spec.rb
+++ spec/helpers/soldsie_s3_helper_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 require 'language_pack/helpers/soldsie_s3_helper'
+require 'base64'
 
 describe 'Soldsie S3 Helper' do
 
   let(:bigquery_local_key_file) { '/tmp/soldsie-event-tracking.p12' }  
 
   before(:each) do    
-    expect(ENV).to receive(:[]).with('AWS_ACCESS_KEY_ID').and_return('AKIAI3MMTAP6CG5HONOQ')
-    expect(ENV).to receive(:[]).with('AWS_SECRET_ACCESS_KEY').and_return('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')
+    expect(ENV).to receive(:[]).with('AWS_ACCESS_KEY_ID').and_return(Base64.decode64('QUtJQUkzTU1UQVA2Q0c1SE9OT1E=\n'))
+    expect(ENV).to receive(:[]).with('AWS_SECRET_ACCESS_KEY').and_return(Base64.decode64('Q3VnSWhxVWNzSnFoQmkyWFhLUVJlMTVUYjBXemNuQUNqaTJDZXZCVg==\n'))
     expect(ENV).to receive(:[]).with('BIGQUERY_KEY_S3_BUCKET').and_return('soldsie-staging-keys')
     expect(ENV).to receive(:[]).with('BIGQUERY_KEY_S3_PATH').and_return('bigquery/Soldsie-event-tracking-staging.p12')
     expect(ENV).to receive(:[]).with('BIGQUERY_KEY_FILENAME').and_return('soldsie-event-tracking.p12')
