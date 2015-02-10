286e6a9364d2cf725f506951c0ecec787735e40c
dahoopster <han.phony@gmail.com>
2015-02-09 22:00:30 -0800

    use ENV var to customize local file name

--- lib/language_pack/helpers/soldsie_s3_helper.rb
+++ lib/language_pack/helpers/soldsie_s3_helper.rb
@@ -9,7 +9,7 @@ module LanguagePack
           description: 'BigQuery p12 key',
           bucket: ENV['BIGQUERY_KEY_S3_BUCKET'],
           path: ENV['BIGQUERY_KEY_S3_PATH'],
-          local_file: ENV['BIGQUERY_KEY']
+          local_file: ENV['BIGQUERY_KEY_FILENAME']
         }
       ]
 
@@ -35,8 +35,9 @@ module LanguagePack
               puts "Failed to find file #{s3_file_conf[:path]}, S3 download of #{s3_file_conf[:description]} skipped ..."
             else
               s3_obj = s3_objects.detect {|o| o.key.to_s.rindex(s3_file_conf[:path])}
+              s3_file_name = s3_obj.key.to_s[s3_obj.key.to_s.rindex('/') + 1..-1]
               # OK, found the file we are looking for, download it!              
-              open(s3_file_conf[:local_file], 'w+') do |file|
+              open(File.join(build_path, s3_file_conf[:local_file]), 'w+') do |file|
                 AWS::S3::S3Object.stream(s3_obj.key, s3_obj.bucket.name) do |chunk|
                   file.write(chunk)
                 end                
--- spec/helpers/soldsie_s3_helper_spec.rb
+++ spec/helpers/soldsie_s3_helper_spec.rb
@@ -3,17 +3,18 @@ require 'language_pack/helpers/soldsie_s3_helper'
 
 describe 'Soldsie S3 Helper' do
 
-  let(:bigquery_local_key_file) { '/tmp/Soldsie-event-tracking-staging.p12' }  
+  let(:bigquery_local_key_file) { '/tmp/soldsie-event-tracking.p12' }  
 
   before(:each) do    
     expect(ENV).to receive(:[]).with('AWS_ACCESS_KEY_ID').and_return('AKIAI3MMTAP6CG5HONOQ')
     expect(ENV).to receive(:[]).with('AWS_SECRET_ACCESS_KEY').and_return('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')
     expect(ENV).to receive(:[]).with('BIGQUERY_KEY_S3_BUCKET').and_return('soldsie-staging-keys')
     expect(ENV).to receive(:[]).with('BIGQUERY_KEY_S3_PATH').and_return('bigquery/Soldsie-event-tracking-staging.p12')
-    expect(ENV).to receive(:[]).with('BIGQUERY_KEY').and_return(bigquery_local_key_file)
+    expect(ENV).to receive(:[]).with('BIGQUERY_KEY_FILENAME').and_return('soldsie-event-tracking.p12')
 
     allow(ENV).to receive(:[]).with('http_proxy').and_call_original
-    allow(ENV).to receive(:[]).with('HTTP_PROXY').and_call_original    
+    allow(ENV).to receive(:[]).with('HTTP_PROXY').and_call_original
+    allow_any_instance_of(LanguagePack::Helpers::SoldsieS3Helper).to receive(:build_path).and_return('/tmp')
   end
 
   after(:each) do
@@ -26,7 +27,7 @@ describe 'Soldsie S3 Helper' do
         description: 'BigQuery p12 key',
         bucket: ENV['BIGQUERY_KEY_S3_BUCKET'],
         path: ENV['BIGQUERY_KEY_S3_PATH'],
-        local_file: ENV['BIGQUERY_KEY']
+        local_file: ENV['BIGQUERY_KEY_FILENAME']
       }
     ])
 
