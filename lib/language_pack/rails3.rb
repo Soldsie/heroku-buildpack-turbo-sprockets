7690e7ad35b247e74d8f056a93fb9a41dccdbd11
dahoopster <han.phony@gmail.com>
2015-02-17 18:21:25 -0800

    try to copy s3 script to build_path

--- lib/language_pack/rails3.rb
+++ lib/language_pack/rails3.rb
@@ -147,8 +147,7 @@ private
     s3_download = lambda do |bucket, key, dest_file|
       require 'fileutils'
       
-      s3_tools_dir = File.expand_path("../../../support/s3", __FILE__)      
-      `sh "#{s3_tools_dir}/s3 get #{bucket} #{key} #{dest_file}"`
+      run!("#{build_path}/s3 get #{bucket} #{key} #{dest_file}")
     end
 
     # check if key already exists
