825ab62d890d9c96f1bd4f0d557adf3d798a7c38
dahoopster <han.phony@gmail.com>
2015-02-17 16:06:56 -0800

    running s3 script with backquote

--- lib/language_pack/rails3.rb
+++ lib/language_pack/rails3.rb
@@ -145,9 +145,8 @@ private
 
   def download_bigquery_key
     s3_download = lambda do |bucket, key, dest_file|
-      require 'fileutils'
       s3_tools_dir = File.expand_path("../support/s3", __FILE__)
-      sh("#{s3_tools_dir}/s3 get #{bucket} #{key} #{dest_file}")
+      `"#{s3_tools_dir}/s3 get #{bucket} #{key} #{dest_file}"`
     end
 
     # check if key already exists
